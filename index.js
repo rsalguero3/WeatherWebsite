01660b294b4d6e9127ea2ad64a5635adc5a0ed83
Ricardo Salguero <rsalguero3@yahoo.com>
2017-12-02 17:33:53 -0500

    Completed template for simple CLI weather app

---
+++ index.js
@@ -0,0 +1,17 @@
+const request = require('request');
+const argv = require('yargs').argv;
+
+let apiKey = 'ea31099a39c0d27a9e12db63ec106e40';
+let city = argv.c || 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+request(url, function (err, response, body) {
+    if(err){
+        console.log('error:', err);
+    }
+    else {
+        let weather = JSON.parse(body);
+        let message = `It's ${weather.main.temp} degrees in ${weather.name}`;
+        console.log(message);
+    }
+});
